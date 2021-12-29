3f0fe8dd5804effeaa690dd0b3e4936ad2d93a7f
AngryCloudEver <68358555+AngryCloudEver@users.noreply.github.com>
2021-12-29 16:38:17 +0700

    Dialogue System Tutorial Pt.2

--- Assets/Codes/DialogueCollisionChecker.cs
+++ Assets/Codes/DialogueCollisionChecker.cs
@@ -35,16 +35,31 @@ public class DialogueCollisionChecker : MonoBehaviour
 
         // For Dialgoue
         theText.text = "Hey, wake up! You need to go, right?";
-        StartCoroutine(Timer(5f, 0));
+        StartCoroutine(Timer(4f, 0));
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(timerRuning == false && dialogue0 == 1)
+        if(timerRunning == false && dialogue0 == 1)
         {
             theText.text = "5 minute...";
+            StartCoroutine(Timer(2.5f, 0));
+        }
+        else if(timerRunning == false && dialogue0 == 2)
+        {
+            theText.text = "What? No, you can't, you have to go now!";
             StartCoroutine(Timer(5f, 0));
+        }
+        else if (timerRunning == false && dialogue0 == 3)
+        {
+            theText.text = "Where are we going?";
+            StartCoroutine(Timer(3f, 0));
+        }
+        else if (timerRunning == false && dialogue0 == 4)
+        {
+            theText.text = "Where are we going...? What!? DonÅft you remember? WerenÅft you the one who told me to prepare for today? What has happened to you?";
+            StartCoroutine(Timer(10f, 0));
         }
     }
 
@@ -60,12 +75,12 @@ public class DialogueCollisionChecker : MonoBehaviour
     }
 
     // Timer
-    private bool timerRuning = false;
+    private bool timerRunning = false;
     IEnumerator Timer(float time, int dialogueNumber)
     {
-        timerRuning = true;
+        timerRunning = true;
         yield return new WaitForSeconds(time);
         if (dialogueNumber == 0) dialogue0++;
-        timerRuning = false;
+        timerRunning = false;
     }
 }
