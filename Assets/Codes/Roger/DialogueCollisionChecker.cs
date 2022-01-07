78bce1d18fe116da3bb2d5c91083f8a44f8fdfad
AngryCloudEver <68358555+AngryCloudEver@users.noreply.github.com>
2022-01-07 15:21:46 +0700

    Dialogue and Fade In

--- Assets/Codes/Roger/DialogueCollisionChecker.cs
+++ Assets/Codes/Roger/DialogueCollisionChecker.cs
@@ -9,6 +9,7 @@ public class DialogueCollisionChecker : MonoBehaviour
     public GameObject textBox;
     public GameObject textContent;
 
+    public Image blackScreen;
     public RawImage theImage;
     private Text theText;
     public Texture[] dialogueImages = new Texture[3];
@@ -16,7 +17,6 @@ public class DialogueCollisionChecker : MonoBehaviour
 
     private int dialogue0;
     private bool dialogue1key;
-
 
     void Awake()
     {
@@ -26,8 +26,7 @@ public class DialogueCollisionChecker : MonoBehaviour
             dialogueBox.transform.localScale = new Vector3(0, 0, 0);
         }
 
-        // textBox.transform.localScale = new Vector3(0, 0, 0);
-        // textContent.transform.localScale = new Vector3(0, 0, 0);
+        StartCoroutine(HideDialogueBox(0f));
     }
 
     // Start is called before the first frame update
@@ -41,8 +40,8 @@ public class DialogueCollisionChecker : MonoBehaviour
         theImage.texture = dialogueImages[currentImage];
 
         // For Dialgoue
-        theText.text = "Hey, wake up! You need to go, right?";
-        StartCoroutine(Timer(4f, 0, 2));
+        StartCoroutine(Timer(2f, 0, 0, "Hey, wake up! You need to go, right?"));
+        StartCoroutine(ShowDialogueBox(2f));
     }
 
     // Update is called once per frame
@@ -50,23 +49,20 @@ public class DialogueCollisionChecker : MonoBehaviour
     {
         if(timerRunning == false && dialogue0 == 1)
         {
-            theText.text = "5 minute...";
-            StartCoroutine(Timer(2.5f, 0, 0));
+            StartCoroutine(Timer(2f, 0, 2, "5 minutes..."));
         }
         else if(timerRunning == false && dialogue0 == 2)
         {
-            theText.text = "What? No, you can't, you have to go now!";
-            StartCoroutine(Timer(5f, 0, 2));
+            StartCoroutine(Timer(2.5f, 0, 0, "What? No, you can't, you have to go now!"));
         }
         else if (timerRunning == false && dialogue0 == 3)
         {
-            theText.text = "Where are we going?";
-            StartCoroutine(Timer(3f, 0, 0));
+            StartCoroutine(FadeBlackIn(2));
+            StartCoroutine(Timer(5f, 0, 2, "Where are we going?"));
         }
         else if (timerRunning == false && dialogue0 == 4)
         {
-            theText.text = "Where are we going...? What!? DonÅft you remember? WerenÅft you the one who told me to prepare for today? What has happened to you?";
-            StartCoroutine(Timer(10f, 0, -1));
+            StartCoroutine(Timer(3f, 0, 0, "Where are we going...? What!? DonÅft you remember? WerenÅft you the one who told me to prepare for today? What has happened to you?"));
         }
     }
 
@@ -81,9 +77,9 @@ public class DialogueCollisionChecker : MonoBehaviour
         }
     }
 
-    // Timer
+    // Timer & Dialogue Changes
     private bool timerRunning = false;
-    IEnumerator Timer(float time, int dialogueNumber, int changeImage)
+    IEnumerator Timer(float time, int dialogueNumber, int changeImage, string dialogue)
     {
         timerRunning = true;
         yield return new WaitForSeconds(time);
@@ -92,6 +88,55 @@ public class DialogueCollisionChecker : MonoBehaviour
         if(changeImage != -1)
         {
             theImage.texture = dialogueImages[changeImage];
+            theText.text = dialogue;
         }
+    }
+
+    IEnumerator FadeBlackIn(int fadeSpeed)
+    {
+        Color objectColor = blackScreen.GetComponent<Image>().color;
+        float fadeAmount;
+
+        while (blackScreen.GetComponent<Image>().color.a > 0)
+        {
+            fadeAmount = objectColor.a - (fadeSpeed * Time.deltaTime);
+
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            blackScreen.GetComponent<Image>().color = objectColor;
+
+            yield return null;
+        }
+    }
+
+    IEnumerator FadeBlackOut(int fadeSpeed)
+    {
+        Color objectColor = blackScreen.GetComponent<Image>().color;
+        float fadeAmount;
+
+        while (blackScreen.GetComponent<Image>().color.a < 1)
+        {
+            fadeAmount = objectColor.a + (fadeSpeed * Time.deltaTime);
+
+            objectColor = new Color(objectColor.r, objectColor.g, objectColor.b, fadeAmount);
+            blackScreen.GetComponent<Image>().color = objectColor;
+
+            yield return null;
+        }
+    }
+
+    IEnumerator HideDialogueBox(float wait = 0f)
+    {
+        yield return new WaitForSeconds(wait);
+
+        textBox.transform.localScale = new Vector3(0, 0, 0);
+        textContent.transform.localScale = new Vector3(0, 0, 0);
+    }
+
+    IEnumerator ShowDialogueBox(float wait = 0f)
+    {
+        yield return new WaitForSeconds(wait);
+
+        textBox.transform.localScale = new Vector3(1, 1, 1);
+        textContent.transform.localScale = new Vector3(1, 1, 1);
     }
 }
